46ab22933ea0757ce6732384e6b41adfddefb521
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-11-16 23:21:35 +0530

    added company card

--- src/Home/components/CompanyCard.js
+++ src/Home/components/CompanyCard.js
@@ -1,6 +1,28 @@
+import { useEffect,useState } from "react"
 const CompanyCard = ()=>{
+    const [orgData,setOrgData] = useState(null);
+    useEffect(()=>{
+        fetchOrgBasedProfileInfo()
+    },[])
 
-    return (
+    const fetchOrgBasedProfileInfo=async()=>{
+     /*
+     * @me todo - fetching data from back end to get company related profile
+     * integrate with mongo db (mongoose orm) or supabase sql (prisma orm)
+     * amazon s3 bucket to show images for the proof of joining or achievement if any
+     */ 
+      setOrgData(null);
+    }
+
+    return orgData==null ?
+    (
+        <>
+          <div>
+            to do api call 
+          </div>
+        </>
+    )
+    :(
         <div>
             <div>
                 <h1>Company Name</h1>
