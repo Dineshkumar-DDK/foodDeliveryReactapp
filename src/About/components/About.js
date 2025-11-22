f58e6ef8a3a321a413596395b53f88a000f1975f
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-11-22 21:26:29 +0530

    class component example in eact js

--- src/About/components/About.js
+++ src/About/components/About.js
@@ -1,8 +1,14 @@
-export const About = () => {
-    return (
-        <div>
-            This is Dinesh Kumar.
-            Iam a software Engineer...
-        </div>
-    )
+import React from "react";
+class About extends React.Component{
+
+    render(){
+       return ( <div>
+            Hi this is dinesh 
+            Software Developer
+            nearly 2 years of experience
+            1 year of professional experience
+        </div>)
+    }
 }
+
+export {About}
