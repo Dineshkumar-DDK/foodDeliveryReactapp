7c482f2111c3f85774d45974151704dc22d2c4bc
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-11-22 22:13:56 +0530

    props usage in constructor  in  class component

--- App.js
+++ App.js
@@ -52,7 +52,7 @@ const appRouter = createBrowserRouter([
             },
             {
                 path: '/about',
-                element: <About />
+                element: <About name={"dinesh..kumar."} />
             },
             {
                 path:'/contact',
--- src/About/components/About.js
+++ src/About/components/About.js
@@ -1,9 +1,12 @@
 import React from "react";
 class About extends React.Component{
-
+    constructor(props){
+        super(props)
+        console.log(props)
+    }
     render(){
        return ( <div>
-            Hi this is dinesh 
+            Hi this is {this.props.name} 
             Software Developer
             nearly 2 years of experience
             1 year of professional experience
